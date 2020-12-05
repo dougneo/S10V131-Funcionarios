a52826c7223d98a963712c77dd57341ccc2909ae
Douglas <doug.neo@gmail.com>
2020-12-05 13:40:11 -0300

    Adicionar arquivos de projeto.

---
+++ S10V131 Funcionarios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S10V131_Funcionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
