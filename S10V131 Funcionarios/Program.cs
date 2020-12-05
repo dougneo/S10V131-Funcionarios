0e4365d4bc3018c95ea9010b705be91b46d7d17c
Douglas <doug.neo@gmail.com>
2020-12-05 15:49:30 -0300

    Configurada as classes, e iniciado o programa principal.

--- S10V131 Funcionarios/Program.cs
+++ S10V131 Funcionarios/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using S10V131_Funcionarios.Entities;
+using System.Globalization;
 
 namespace S10V131_Funcionarios
 {
@@ -6,7 +9,37 @@ namespace S10V131_Funcionarios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+
+                Console.Write("Outsourced (y/n)? ");
+                string outsourced = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (outsourced == "y" && outsourced == "Y")
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+                }
+
+
+
+            }
         }
     }
 }
